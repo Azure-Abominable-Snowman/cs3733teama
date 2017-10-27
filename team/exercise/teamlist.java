f95d5852cdbb769622400c10d641e8b9a715ff7a
YizhenW <33132821+YizhenW@users.noreply.github.com>
2017-10-27 14:40:44 -0400

    Update teamlist.java

--- team/exercise/teamlist.java
+++ team/exercise/teamlist.java
@@ -5,16 +5,7 @@ package team.exercise;
  */
 public class teamlist {
     public static void main(String[] args){
-        System.out.println("Alissa");
-        System.out.println("Alex");
-        System.out.println("Kent");
-        System.out.println("Ray");
-        System.out.println("Meghan");
-        System.out.println("Ruyue");
-        System.out.println("Yuanda(Danny)");
-        System.out.println("Jerry");
-        System.out.println("Jake Pardue");
-        System.out.println("Qingyuan (Leo)");
+        System.out.println("Yizhen");
         
 
     }
