900d5c9476198b4f3c444cdfbdfd7f26f8490013
Kent <klibby@wpi.edu>
2017-10-27 02:24:08 -0400

    Adds Kent's name to list of names

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        //TODO figure out how to make the name of the file teamlist.java
         System.out.println("Alex Krasa");
-
-
+        System.out.println("Alissa Ostanpenko");
+        System.out.println("Kent Libby");
     }
 
 }
