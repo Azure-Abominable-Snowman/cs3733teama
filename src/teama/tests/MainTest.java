b9535b09a83aef7bf3761fd2e6fbc45997352e89
Raymond Schade <rayschade@gmail.com>
2017-11-03 21:47:11 -0400

    Commit with new repo for Project C and TravisCI

---
+++ src/teama/tests/MainTest.java
@@ -0,0 +1,14 @@
+package teama.tests;
+
+class MainTest {
+    @org.junit.jupiter.api.Test
+    void start() {
+        assert true;
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        assert true;
+    }
+
+}
