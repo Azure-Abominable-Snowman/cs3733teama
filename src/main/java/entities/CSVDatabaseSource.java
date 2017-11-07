511e9fa7244a896521a759f7c7cbed440edc8f1d
Raymond Schade <rayschade@gmail.com>
2017-11-07 16:41:47 -0500

    Continuing to work on the CSV output, problems with the addition/updating of specific CSV lines.

--- src/main/java/entities/CSVDatabaseSource.java
+++ src/main/java/entities/CSVDatabaseSource.java
@@ -1,27 +1,22 @@
 package entities;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class CSVDatabaseSource implements MapDataSource {
 
     private HashMap<String, MapNode> nodeMap = new HashMap<>();
     private HashMap<String, MapEdge> edgeMap = new HashMap<>();
+    private String nodeFilename, edgeFilename;
 
     public CSVDatabaseSource(String nodeFilename, String edgeFilename) {
+        this.nodeFilename = nodeFilename;
+        this.edgeFilename = edgeFilename;
         ArrayList<List<String>> nodeData = parseCSVFile(nodeFilename);
         for (List<String> row : nodeData.subList(1, nodeData.size())) {
             // Iterate through each row and make a node object
             // for each one and put it into the hashmap
-            MapNode n = new MapNode(row.get(0),
-                            new Location(Integer.parseInt(row.get(1)), Integer.parseInt(row.get(2)),
-                            row.get(3), row.get(4)), NodeType.valueOf(row.get(5)), row.get(6), row.get(7), row.get(8), null);
+            MapNode n = nodeListToObj(row);
             nodeMap.put(row.get(0), n);
         }
 
@@ -30,7 +25,7 @@ public class CSVDatabaseSource implements MapDataSource {
             // Iterate through each row and make an edge object
             // for each one and put it into the hashmap
             // Look up corresponding edges in the node hashmap
-            MapEdge e = new MapEdge(row.get(0), nodeMap.get(row.get(1)), nodeMap.get(row.get(2)), 0);
+            MapEdge e = edgeListToObj(row);
             edgeMap.put(row.get(0), e);
         }
     }
@@ -43,10 +38,7 @@ public class CSVDatabaseSource implements MapDataSource {
     private ArrayList<List<String>> parseCSVFile(String filename) {
         BufferedReader nodeReader;
         try {
-            // FileReader reads text files in the default encoding.
             FileReader fileReader = new FileReader(filename);
-
-            // Always wrap FileReader in BufferedReader.
             nodeReader =  new BufferedReader(fileReader);
 
         }
@@ -61,20 +53,34 @@ public class CSVDatabaseSource implements MapDataSource {
 
         try {
             while(nodeReader.ready()) {
-                // Add a new parsed CSV line to the list
-                String[] sep = nodeReader.readLine().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-                // separated CSV string may still have trailing and starting quotes, remove these
-                // TODO: Be able to parse multi-layer quotes, only remove the outer layer.
-                for(int i = 0; i < sep.length; i++) {
-                    sep[i] = sep[i].replace("\"", "");
-                }
-                data.add(Arrays.asList(sep));
+                data.add(splitCSVLine(nodeReader.readLine()));
             }
             nodeReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return data;
+    }
+
+    private List<String> splitCSVLine(String line) {
+        // Add a new parsed CSV line to the list
+        String[] sep = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+        // separated CSV string may still have trailing and starting quotes, remove these
+        // TODO: Be able to parse multi-layer quotes, only remove the outer layer.
+        for(int i = 0; i < sep.length; i++) {
+            sep[i] = sep[i].replace("\"", "");
+        }
+        return Arrays.asList(sep);
+    }
+
+    private MapNode nodeListToObj(List<String> row) {
+        return new MapNode(row.get(0),
+                new Location(Integer.parseInt(row.get(1)), Integer.parseInt(row.get(2)),
+                        row.get(3), row.get(4)), NodeType.valueOf(row.get(5)), row.get(6), row.get(7), row.get(8), null);
+    }
+
+    private MapEdge edgeListToObj(List<String> row) {
+        return new MapEdge(row.get(0), nodeMap.get(row.get(1)), nodeMap.get(row.get(2)), 0);
     }
 
     @Override
@@ -96,34 +102,79 @@ public class CSVDatabaseSource implements MapDataSource {
     public void addNode(MapNode node) {
         nodeMap.put(node.getId(), node);
         // Open node file, and put the updated CSV into it
-        writeNodes();
+        try {
+            writeNode(node, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeNode(String id) {
-        nodeMap.remove(id);
         // Open the node file, and put the updated CSV into it
-        writeNodes();
+        try {
+            writeNode(nodeMap.get(id), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        nodeMap.remove(id);
     }
 
     @Override
     public void addEdge(MapEdge edge) {
         edgeMap.put(edge.getId(), edge);
         // Open the edge file, and put the updated CSV into it
-        writeEdges();
+        writeEdge(edge, false);
     }
 
-    private void writeNodes() {
-        String header = "nodeID,xcoord,ycoord,floor,building,nodeType,longName,shortName,teamAssigned";
+    private void writeNode(MapNode node, boolean delete) throws IOException {
+        try {
+            File file = new File(nodeFilename);
+            if(!file.exists()) {
+                // If the file exists something must of been changed
+                throw new IOException();
+            }
+            // Open the file and find the line number to replace or remove
+            RandomAccessFile fileReader = new RandomAccessFile(nodeFilename, "rw");
+            String currLine = fileReader.readLine();
+            int numLines = 1;
+            while(currLine != null && !splitCSVLine(currLine).get(0).equals(node.getId())) {
+                currLine = fileReader.readLine();
+                numLines++;
+            }
+            // At this point the file pointer is at the end of the line we want to change
+
+            if(currLine != null) { // exists in the file, need to replace the line
+                StringBuilder newLine = new StringBuilder(node.toCSV());
+                for (int i = 0; i < currLine.length() - node.toCSV().length(); i++) {
+                    newLine.append(" ");
+                }
+                System.out.println("NEW LINE: " + newLine.toString() + "|");
+
+                System.out.println(numLines);
+                fileReader.seek(0);
+                for(int i = 0; i < numLines-1; i++) {
+                    fileReader.readLine();
+                }
+                for(byte b : "55".getBytes()) {
+                    fileReader.writeByte(b);
+                }
+            } else { // Doesn't exist in the file, append to the end
+
+            }
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
         for(MapNode n : nodeMap.values()) {
             System.out.println(n.toCSV());
         }
     }
 
-    private void writeEdges() {
-        String header = "edgeID,startNode,endNode";
-        for(MapEdge e : edgeMap.values()) {
+    private void writeEdge(MapEdge edge, boolean delete) {
+        /*for(MapEdge e : edgeMap.values()) {
             System.out.println(e.toCSV());
-        }
+        }*/
     }
 }
--- src/main/java/entities/Main.java
+++ src/main/java/entities/Main.java
@@ -9,14 +9,14 @@ public class Main {
     public static void main(String[] args) {
         // Create a JavaDB source and a CSV source
         MapDataSource dataSource = new JavaDatabaseSource("jdbc:derby://localhost:1527/testdb", "TEST_NODES", "TEST_EDGES");
-        MapNode testNode = new MapNode("ACONF0010G", new Location(1671,2648,"G", "BTM"),
+        MapNode testNode = new MapNode("WELEV00H01", new Location(2735,2648,"G", "BTM"),
                 NodeType.CONF, "Schlagler Lobby", "Schlagler Innovation Lobby","Team A", null);
         dataSource.addNode(testNode);
         // Test CSV
-        System.out.println(dataSource.getNode("ACONF0010G").toCSV());
+        System.out.println(dataSource.getNode("WELEV00H01").toCSV());
         MapDataSource csv = new CSVDatabaseSource("C:\\Users\\raysc\\Documents\\CS3733\\cs3733teama\\csvdata\\MapWnodes.csv",
                                                   "C:\\Users\\raysc\\Documents\\CS3733\\cs3733teama\\csvdata\\MapWedges.csv");
         csv.addNode(testNode);
-        System.out.println("T: "+csv.getNode("ACONF0010G").toCSV());
+        System.out.println("T: "+csv.getNode("WELEV00H01").toCSV());
     }
 }
