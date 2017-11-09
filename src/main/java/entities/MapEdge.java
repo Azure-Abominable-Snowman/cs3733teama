c31943d47310c5ba1761ab5c9dad6779aa3a8e4f
Leo Chen <qchen3@gmail.com>
2017-11-09 16:20:53 -0500

    Formatting Change, using IntelliJ auto formatting

--- src/main/java/entities/MapEdge.java
+++ src/main/java/entities/MapEdge.java
@@ -7,21 +7,22 @@ public class MapEdge {
 
     /**
      * Creates an edge on the graph, the weight is assumed to be the euclidean distance between the start and end nodes.
+     *
      * @param id
      * @param start
      * @param end
      */
     public MapEdge(String id, MapNode start, MapNode end) {
         this(id, start, end,
-                Math.sqrt(Math.pow(end.getCoordinate().getxCoord()-start.getCoordinate().getxCoord(), 2)+
-                Math.pow(end.getCoordinate().getyCoord()-start.getCoordinate().getyCoord(), 2)));
+                Math.sqrt(Math.pow(end.getCoordinate().getxCoord() - start.getCoordinate().getxCoord(), 2) +
+                        Math.pow(end.getCoordinate().getyCoord() - start.getCoordinate().getyCoord(), 2)));
     }
 
     public MapEdge(String id, MapNode start, MapNode end, double weight) {
-        if(start == null) {
+        if (start == null) {
             System.out.println("Tried to set a null start");
         }
-        if(end == null) {
+        if (end == null) {
             System.out.println("Tried to set a null end");
         }
         this.start = start;
@@ -35,7 +36,7 @@ public class MapEdge {
     }
 
     public void setStart(MapNode start) {
-        if(start == null) {
+        if (start == null) {
             System.out.println("Tried to set a null start");
         }
         this.start = start;
@@ -47,15 +48,14 @@ public class MapEdge {
     }
 
     public void setEnd(MapNode end) {
-        if(end == null) {
+        if (end == null) {
             System.out.println("Tried to set a null end");
         }
         this.end = end;
     }
 
-    public MapNode getAdjacentNode(MapNode origionalNode)
-    {
-        if (origionalNode==this.start) return this.end;
+    public MapNode getAdjacentNode(MapNode originalNode) {
+        if (originalNode == this.start) return this.end;
         else return this.start;
     }
 
