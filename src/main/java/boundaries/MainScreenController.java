74bc14998fc993c6b5d117434e0928b59a4e71f7
Yuanda(Danny) Song <ysong5@wpi.edu>
2017-11-12 17:37:29 -0500

    test

--- src/main/java/boundaries/MainScreenController.java
+++ src/main/java/boundaries/MainScreenController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
+import javax.swing.text.html.ImageView;
 
 
 public class MainScreenController {
@@ -17,6 +18,8 @@ public class MainScreenController {
     Button editMap;
     @FXML
     Button LogIn;
+    @FXML
+    ImageView maps;
 
     @FXML
     private void requestClick(ActionEvent event){
