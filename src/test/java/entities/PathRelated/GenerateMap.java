d54045a38786b130980270de2ebd20369e78221d
ruyuewang <rwang5@wpi.edu>
2017-11-12 18:48:50 -0500

    create helper for making edge between two nodes

--- src/test/java/entities/PathRelated/GenerateMap.java
+++ src/test/java/entities/PathRelated/GenerateMap.java
@@ -1,6 +1,7 @@
 package entities.PathRelated;
 
 import entities.Location;
+import entities.MapEdge;
 import entities.MapNode;
 import entities.NodeType;
 
@@ -20,23 +21,31 @@ public class GenerateMap {
 
     }
 
-    public MapNode[][] GenerateV1(int sizeX,int sizeY)
+    public MapNode[][] GenerateV1(int sizeX,int sizeY) // assume bigger then 20*20
     {
 
-        MapNode[][] map = new MapNode[sizeX][sizeY];
+        MapNode[][] map = new MapNode[sizeX][sizeY]; // allocate new map
 
         String str  = new String();
-        Location tempLoc = new Location(1,1,"1","a");
-        for(int i = 0;i< sizeX;i++){
+        Location tempLoc = new Location(1,1,"1","a");// create location
+
+        for(int i = 0;i< sizeX;i++){    // start fill in the map
             for (int j = 0; j < sizeY; j++)
             {
-                tempLoc.setxCoord(i);
+                tempLoc.setxCoord(i);   // put in correct coord
                 tempLoc.setyCoord(j);
-                str=str.format("C %d R %d",i,j);
+                str=str.format("C %d R %d",i,j);    // create correct name
                 map[i][j]= new MapNode(str,tempLoc, NodeType.HALL,str
-                        ,str,"A");
+                        ,str,"A");  // create the Node in the map
             }
         }
+
+        // Done creating the Nodes, Start making edges.
+        linkNodes(map[0][0], map[0][1],1);
+        linkNodes(map[0][2], map[0][3],2);
+        linkNodes(map[0][4], map[0][5],3);
+
+
         return map;
     }
 
@@ -44,6 +53,23 @@ public class GenerateMap {
     {
         return null;
     }
+
+
+    //////helper
+
+    /**
+     * This is the helper function to create the edge between two nodes and add the edge to two nodes.
+     * @param node1 is the first node.
+     * @param node2 is the second node.
+     * @param weight is the weight between two nodes.
+     * @return This function returns void.
+     */
+    public void linkNodes(MapNode node1, MapNode node2, double weight){
+        MapEdge edge = new MapEdge("e1", node1, node2, weight);
+         node1.addEdge(edge);
+         node2.addEdge(edge);
+    }
+
 
 
 
