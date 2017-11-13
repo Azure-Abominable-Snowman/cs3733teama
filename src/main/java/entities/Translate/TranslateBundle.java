592e10ef31bff94a5566bbd132c5d01006c61496
Alex K <ajkrasa@wpi.edu>
2017-11-13 17:38:32 -0500

    Began working of Translator Application V4.5 Trying to get SceneBuilder Inter. working

--- src/main/java/entities/Translate/TranslateBundle.java
+++ src/main/java/entities/Translate/TranslateBundle.java
@@ -7,13 +7,13 @@ import java.util.ResourceBundle;
 public class TranslateBundle {
 
     Locale english = new Locale("en", "US");
-    ResourceBundle label1 = ResourceBundle.getBundle("Translate.properties",english);
+    ResourceBundle label1 = ResourceBundle.getBundle("Translation.properties",english);
 
     Locale spanish = new Locale("es", "US");
-    ResourceBundle label2 = ResourceBundle.getBundle("Translate.properties",spanish);
+    ResourceBundle label2 = ResourceBundle.getBundle("Translation.properties",spanish);
 
     Locale french = new Locale("fr", "US");
-    ResourceBundle label3 = ResourceBundle.getBundle("Translate.properties",french);
+    ResourceBundle label3 = ResourceBundle.getBundle("Translation.properties",french);
 
 
 
