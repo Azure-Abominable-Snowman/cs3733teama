f454ea4f2e690249a8a602852da18f7e58e41730
Leo Chen <qchen3@wpi.edu>
2017-11-13 09:59:50 -0500

    factor out the map filling bit in mapGenerating.

--- src/test/java/entities/PathRelated/GenerateMap.java
+++ src/test/java/entities/PathRelated/GenerateMap.java
@@ -30,20 +30,7 @@ public class  GenerateMap  {
         if(sizeY<5 ) sizeY =5; // make sure no sizes are smaller then 5;
 
         MapNode[][] map = new MapNode[sizeX][sizeY]; // allocate new map
-
-        String str  = new String();
-        Location tempLoc = new Location(1,1,"1","a");// create location
-
-        for(int i = 0;i< sizeX;i++){    // start fill in the map
-            for (int j = 0; j < sizeY; j++)
-            {
-                tempLoc.setxCoord(i);   // put in correct coord
-                tempLoc.setyCoord(j);
-                str=str.format("C %d R %d",i,j);    // create correct name
-                map[i][j]= new MapNode(str,tempLoc, NodeType.HALL,str
-                        ,str,"A");  // create the Node in the map
-            }
-        }
+        fillMap (map,sizeX,sizeY);
         // Done creating the Nodes, Start making edges.
         linkNodes(map[0][0], map[0][1],1);
         linkNodes(map[0][1], map[0][2],2);
@@ -83,20 +70,7 @@ public class  GenerateMap  {
         if(sizeY<5 ) sizeY =5; // make sure no sizes are smaller then 5;
 
         MapNode[][] map = new MapNode[sizeX][sizeY]; // allocate new map
-
-        String str  = new String();
-        Location tempLoc = new Location(1,1,"1","a");// create location
-
-        for(int i = 0;i< sizeX;i++){    // start fill in the map
-            for (int j = 0; j < sizeY; j++)
-            {
-                tempLoc.setxCoord(i);   // put in correct coord
-                tempLoc.setyCoord(j);
-                str=str.format("C %d R %d",i,j);    // create correct name
-                map[i][j]= new MapNode(str,tempLoc, NodeType.HALL,str
-                        ,str,"A");  // create the Node in the map
-            }
-        }
+        fillMap (map,sizeX,sizeY);
         // Done creating the Nodes, Start making edges.
         linkNodes(map[0][0], map[0][1],1);
         linkNodes(map[0][1], map[0][2],2);
@@ -132,6 +106,29 @@ public class  GenerateMap  {
     ///////////helper////////////
 
     /**
+     * fill the Map up with simple node at type HallWay.
+     * @param map   the map to be filled
+     * @param sizeX is the width of map (can be smaller, but no bigger then actual map)
+     * @param sizeY is the height of map (can be smaller, but no bigger then actual map)
+     */
+    private void fillMap (MapNode[][] map,int sizeX,int sizeY)
+    {
+        String str  = new String();
+        Location tempLoc = new Location(1,1,"1","a");// create location
+        for(int i = 0;i< sizeX;i++){    // start fill in the map
+            for (int j = 0; j < sizeY; j++)
+            {
+                tempLoc.setxCoord(i);   // put in correct coord
+                tempLoc.setyCoord(j);
+                str=str.format("C %d R %d",i,j);    // create correct name
+                map[i][j]= new MapNode(str,tempLoc, NodeType.HALL,str
+                        ,str,"A");  // create the Node in the map
+            }
+        }
+    }
+
+
+    /**
      * This is the helper function to create the edge between two nodes and add the edge to two nodes.
      * @param node1 is the first node.
      * @param node2 is the second node.
@@ -145,6 +142,12 @@ public class  GenerateMap  {
          node2.addEdge(edge);
     }
 
+    /**
+     * This is a helper function that print the maps generated in this class onto commandline
+     * @param map   the map to be printed
+     * @param sizeX is the width of map (can be smaller, but no bigger then actual map)
+     * @param sizeY is the height of map (can be smaller, but no bigger then actual map)
+     */
     public void printMap (MapNode[][] map,int sizeX, int sizeY)
     {
         System.out.printf("    ");
@@ -181,9 +184,12 @@ public class  GenerateMap  {
         }
     }
 
-
-
-    // helper
+    /**
+     *
+     * @param n1 node 1
+     * @param n2 node 2
+     * @return the weight of the edge connecting node 1 and node 2, return -1 when no edge.
+     */
     private double findWeight(MapNode n1,MapNode n2)
     {
         for (MapEdge edge: n1.getEdges()) {
