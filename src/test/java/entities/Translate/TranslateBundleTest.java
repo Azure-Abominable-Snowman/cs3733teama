426a4ff725e3888e288ae8bba66e93c1e84a1080
Alex K <ajkrasa@wpi.edu>
2017-11-13 16:21:02 -0500

    Began working of Translator Application V4.1 With Tests to check on things

---
+++ src/test/java/entities/Translate/TranslateBundleTest.java
@@ -0,0 +1,22 @@
+package entities.Translate;
+
+/*
+import java.util.Locale;
+import java.util.ResourceBundle;
+ */
+
+public class TranslateBundleTest {
+
+
+
+
+    public static void testLang(String[] args) throws Exception {
+
+
+        TranslateBundle.
+
+
+        //System.out.println(label1.getString("label1"));
+    }
+
+}
