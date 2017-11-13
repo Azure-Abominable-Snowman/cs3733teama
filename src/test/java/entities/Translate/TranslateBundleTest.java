991882a353bd7ed9c5ff78dbb3bab9b4e6b4ce6c
Alex K <ajkrasa@wpi.edu>
2017-11-13 16:57:11 -0500

    Began working of Translator Application V4.4 Added possible translation code w/out API (Probably not an easy solution by comparision)/SceneBuilder might have something build into it.

--- src/test/java/entities/Translate/TranslateBundleTest.java
+++ src/test/java/entities/Translate/TranslateBundleTest.java
@@ -1,22 +1,33 @@
 package entities.Translate;
 
-/*
+import org.junit.Test;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
- */
 
 public class TranslateBundleTest {
 
+    ResourceBundle bundleFR = ResourceBundle.getBundle("Translate_fr", Locale.FRANCE);
 
 
+    public static void testLang(String[] args) {
+        ResourceBundle bundle = ResourceBundle.getBundle("Translate");
 
-    public static void testLang(String[] args) throws Exception {
+        ResourceBundle bundleFR = ResourceBundle.getBundle("Translate_fr", Locale.FRANCE);
 
 
-       // TranslateBundle.
+        //System.out.println(bundle.getString("CountryName"));
+        //System.out.println(bundle.getString("CurrencyCode"));
 
-
-        //System.out.println(label1.getString("label1"));
     }
+
+
+
+    @Test
+    public void getLang() throws Exception {
+        //assert
+        System.out.println(bundleFR.getString("Hello"));
+    }
+
 
 }
