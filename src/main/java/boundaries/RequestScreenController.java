3e4471558dcfced31ae32e1537ab42b17d8cf6b4
Jerry <jerryxu1997@hotmail.com>
2017-11-13 17:49:06 -0500

    adds a drop down menu in the form of combo box

--- src/main/java/boundaries/RequestScreenController.java
+++ src/main/java/boundaries/RequestScreenController.java
@@ -1,15 +1,37 @@
 package boundaries;
 
 import controllers.SceneEngine;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 public class RequestScreenController {
     @FXML
     private Button back;
     @FXML
     Button fulfillRequest;
+    @FXML
+    private ComboBox combo;
+    @FXML
+    private void Initialization(){
+        combo.setItems(list);
+    }
+    ObservableList<String> list = FXCollections.observableArrayList("Food","Security","Interpreter","Transportation");
+
+
+
+    @FXML
+    private Button Comfirm;
+
+
+
+
+
+
+
 
     @FXML
     private void backClick(ActionEvent event){
@@ -21,4 +43,12 @@ public class RequestScreenController {
     private void fulfillClick(ActionEvent event){
         SceneEngine.displayFulfillRequest();
     }
+
+
+
 }
+
+
+
+
+
--- src/main/java/entities/Main.java
+++ src/main/java/entities/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -68,4 +69,6 @@ public class Main extends Application{
         }
 
     }
+
+
 }
