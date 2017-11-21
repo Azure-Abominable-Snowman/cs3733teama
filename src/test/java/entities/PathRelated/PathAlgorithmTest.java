1ffa2519801942aad5e1e657c4c3e4907f7b5a32
Leo Chen <qchen3@gmail.com>
2017-11-20 23:54:13 -0500

    Made special test for using == or .equals

--- src/test/java/entities/PathRelated/PathAlgorithmTest.java
+++ src/test/java/entities/PathRelated/PathAlgorithmTest.java
@@ -1,6 +1,6 @@
 package entities.PathRelated;
 
-import com.teama.mapsubsystem.data.MapNode;
+import com.teama.mapsubsystem.data.*;
 import com.teama.mapsubsystem.pathfinding.AStar.AStar;
 import com.teama.mapsubsystem.pathfinding.BreathFrist.BreathFirst;
 import com.teama.mapsubsystem.pathfinding.Path;
@@ -46,8 +46,23 @@ public class PathAlgorithmTest {
             thrown=true;
         }
         assertTrue(thrown);
+    }
 
-
+    @Test
+    /**
+     * Special test which AStar will be given a fake node with right name for the end.
+     * to test if AStar use .equal or == for pointers.
+     */
+    public void AStarPointerEqualTest()
+    {
+        AStar finder = new AStar();
+        MapNode specialNode = new MapNodeData("C 12 R 1",new Location(12,1, Floor.ONE,"a"),
+                NodeType.HALL,"C 12 R 1","C 12 R 1","A");
+        Path result =  finder.generatePath(map1[0][0],specialNode);
+        assertTrue(result.getNodes().contains(map1[0][0])); // check the start
+        assertTrue(result.getNodes().contains(map1[12][1]));// check the end
+        assertTrue(result.getNodes().contains(map1[0][4])); // check some points in the path.
+        assertTrue(result.getNodes().contains(map1[11][4]));
     }
 
     @Test
