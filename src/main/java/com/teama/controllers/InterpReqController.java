f68baae3e7d69f0edaf7acc4873ccc14ec4d73c9
Kent Libby <klibby@wpi.edu>
2017-11-21 12:45:16 -0500

    starts on sidebar controller for requests

---
+++ src/main/java/com/teama/controllers/InterpReqController.java
@@ -0,0 +1,22 @@
+package com.teama.controllers;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import com.teama.requestsubsystem.Language;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class InterpReqController {
+    @FXML
+    AnchorPane parentPane;
+    @FXML
+    JFXTextField txtFamilySize;
+    @FXML
+    JFXComboBox<Language> cmbLang;
+    public InterpReqController(){
+
+    }
+    public AnchorPane getParentPane() {
+        return parentPane;
+    }
+}
