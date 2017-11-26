8d702700f82de0edf593c895a39e5afc9cd57a42
Leo Chen <qchen3@gmail.com>
2017-11-26 16:25:37 -0500

    new structure for textualDirections.

---
+++ src/main/java/com/teama/mapsubsystem/pathfinding/TextualDirection/AngleGenerator.java
@@ -0,0 +1,33 @@
+package com.teama.mapsubsystem.pathfinding.TextualDirection;
+
+import com.teama.mapsubsystem.data.MapNode;
+
+import java.awt.*;
+
+public class AngleGenerator {
+
+    //TODO Make a Node to Point Conversion
+    Point from; // Point where the Node/ Point is measuring from
+
+
+    public float AngleGenerator(Point target) {
+        float angle = (float) Math.toDegrees(Math.atan2(target.y - from.y, target.x - from.x));
+
+        if(angle < 0){
+            angle += 360;
+        }
+        return angle;
+    }
+
+    public static float positionAngle(MapNode start, MapNode end) {
+        float angle = (float) Math.toDegrees(Math.atan2(start.getCoordinate().getyCoord() - end.getCoordinate().getyCoord(),
+                start.getCoordinate().getxCoord() - end.getCoordinate().getxCoord()));
+        if(angle < 0){
+            angle += 360;
+        }
+        return angle;
+    }
+
+
+
+}
