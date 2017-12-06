c048ba78ac28b85da9d5beeea564e098f390ed00
mebroughton <meghanbroughton@gmail.com>
2017-12-06 04:10:24 -0500

    memento update

--- src/main/java/com/teama/refresh/RefreshToMain.java
+++ src/main/java/com/teama/refresh/RefreshToMain.java
@@ -1,10 +1,11 @@
 package com.teama.refresh;
 
+
 import java.awt.event.InputEvent;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
-import com.teama.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,6 +17,23 @@ public class RefreshToMain {
     private List<MementoDesign.Memento> savedStates = new ArrayList<>();
     private Stage primaryStage;
     private Stage currStage = new Stage();
+
+
+    private Stage setPrimaryStage(Stage primaryStage){
+        this.primaryStage = primaryStage;
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/Main Screen"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Brigham and Women's Hospital Kiosk");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            }   catch (IOException e){
+                e.printStackTrace();
+        }
+        return primaryStage;
+    }
 
 
     public void Refresh(InputEvent interaction) {
@@ -31,8 +49,7 @@ public class RefreshToMain {
             } catch (InterruptedException ie) {
                 ie.printStackTrace();
             }
-            // return mementoDesign.restore(savedStates.get(0));
-
+            //return mementoDesign.restore(savedStates.get(0));
         }
     }
 }
