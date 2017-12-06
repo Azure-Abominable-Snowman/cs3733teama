820dc403c7e57ff741d532024fc7ad486e161386
Yuanda(Danny) Song <ysong5@wpi.edu>
2017-12-05 23:51:00 -0500

    Solve conflict

--- src/main/java/com/teama/mapsubsystem/pathfinding/DirectionAdapter.java
+++ src/main/java/com/teama/mapsubsystem/pathfinding/DirectionAdapter.java
@@ -3,5 +3,21 @@ package com.teama.mapsubsystem.pathfinding;
 import com.teama.mapsubsystem.pathfinding.TextualDirection.Direction;
 
 public class DirectionAdapter {
+    private Direction d;
+    private int stepNum;
 
+    public DirectionAdapter(int stepNum, Direction d) {
+        this.d = d;
+        this.stepNum = stepNum;
+    }
+
+    public String getDescription() {
+        return d.getDescription();
+    }
+
+    public String getDistance() {
+        return Integer.toString((int)d.getLengthOfPath());
+    }
+
+    public int getStepNum() { return stepNum; }
 }
