965420274ca32351f9a3625f7ce6a45312b1f4b5
ruyuewang <rwang5@wpi.edu>
2017-12-09 16:16:43 -0500

    create reverse AStar class for longest path

--- src/main/java/com/teama/mapsubsystem/pathfinding/DijkstrasFamily/AStar/BeamSearch.java
+++ src/main/java/com/teama/mapsubsystem/pathfinding/DijkstrasFamily/AStar/BeamSearch.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class BeamSearch extends AStar {
 
-    private HashMap<String,KnownPointA> checkedPoints;
+    private HashMap<String,KnownPointR> checkedPoints;
     private LimitedPriorityQueue limitedQueue;
     private MapNode start, end;
     private  HashMap<String, MapNode> disableNodes ;
@@ -33,10 +33,10 @@ public class BeamSearch extends AStar {
         if (disableNodes==null) this.disableNodes= new HashMap<String, MapNode>();
 
 
-        KnownPointA checking ; // create a temp variable to keep track of which node are we on.
+        KnownPointR checking ; // create a temp variable to keep track of which node are we on.
 
         //Generate Path
-        for(checking = new KnownPointA(start,null,0,calDistance(start,end));
+        for(checking = new KnownPointR(start,null,0,calDistance(start,end));
             !checking.getNode().getId().equals(end.getId());   // reached end
             checking= limitedQueue.pop() // move forward one step
                 )
@@ -63,7 +63,7 @@ public class BeamSearch extends AStar {
      * @param checking is the node currently under examining.
      */
     @Override
-    protected void putNodesIntoQueue (KnownPointA checking)
+    protected void putNodesIntoQueue (KnownPointR checking)
     {
         for(MapEdge e : checking.getEdge()) // putting the adjacentNodes into queue
         {
@@ -72,7 +72,7 @@ public class BeamSearch extends AStar {
             if( !checkedPoints.containsKey(nextNode.getId())) {  // prevent from going to points already been at.
                 int newPastCost = checking.getPastCost() + (int) e.getWeight();
 
-                KnownPointA nextPoint = new KnownPointA(nextNode, checking, newPastCost,
+                KnownPointR nextPoint = new KnownPointR(nextNode, checking, newPastCost,
                         newPastCost + calDistance(nextNode, end)); // Generate a new Point from checking point to add into queue.
                 limitedQueue.insert(nextPoint); // add into queue
             }
@@ -93,7 +93,7 @@ public class BeamSearch extends AStar {
     private class LimitedPriorityQueue
     {
         private int size;
-        private  LinkedList<KnownPointA> list ;
+        private  LinkedList<KnownPointR> list ;
 
         LimitedPriorityQueue(int size)
         {
@@ -105,17 +105,17 @@ public class BeamSearch extends AStar {
          * Put the point into proper position (using cmopareTo) and then delete the last nodes if the list get too long
          * @param point the node need to be insert into the queue.
          */
-        public void insert(KnownPointA point)
+        public void insert(KnownPointR point)
         {
             int position =0;
-            for (KnownPointA i : list) {
+            for (KnownPointR i : list) {
                 if(i.compareTo(point)==1) break;
                 ++position;
             }
             list.add(position,point);
             for(;list.size()>size;list.pollLast()); // cut the size of the list off.
         }
-        public KnownPointA peek()
+        public KnownPointR peek()
         {
             return list.peek();
         }
@@ -124,7 +124,7 @@ public class BeamSearch extends AStar {
          * same as poll used in AStar, just need a different name for Intellij didn't complain
          * @return return the first node in the list.
          */
-        public KnownPointA pop()
+        public KnownPointR pop()
         {
             return list.pop();
         }
---
+++ src/main/java/com/teama/mapsubsystem/pathfinding/DijkstrasFamily/AStar/KnownPointR.java
@@ -0,0 +1,51 @@
+package com.teama.mapsubsystem.pathfinding.DijkstrasFamily.AStar;
+
+import com.teama.mapsubsystem.data.MapNode;
+import com.teama.mapsubsystem.pathfinding.DijkstrasFamily.KnownPoint;
+
+public class KnownPointR extends KnownPoint {
+
+
+    private KnownPointR lastNode;
+    private int completeCost;
+
+    //constructor
+    public KnownPointR(MapNode node, KnownPointR lastNode, int pastCost, int completeCost) {
+        this.node = node;
+        this.lastNode = lastNode;
+        this.pastCost = pastCost;
+        this.completeCost = completeCost;
+    }
+    /**
+     * Implemented to allow PriorityQueue to sort this.
+     * Compare based on the CompleteCost
+     * @param o KnownPoint to compare to itself
+     * @return 0 if equal. 1 if completeCost in this one is bigger, -1 if this one is smaller.
+     */
+    @Override
+    public int compareTo(Object o) {
+        KnownPointR node = (KnownPointR) o;
+        if (this.completeCost > node.completeCost) return 1;
+        else if (this.completeCost == node.completeCost) return 0;
+        else if (this.completeCost < node.completeCost) return -1;
+        return 0;
+    }
+
+    //////////// getter and setter
+    public KnownPointR getLastNode() {
+        return lastNode;
+    }
+
+    public void setLastNode(KnownPointR lastNode) {
+        this.lastNode = lastNode;
+    }
+
+    public int getCompleteCost() {
+        return completeCost;
+    }
+
+    public void setCompleteCost(int completeCost) {
+        this.completeCost = completeCost;
+    }
+
+}
