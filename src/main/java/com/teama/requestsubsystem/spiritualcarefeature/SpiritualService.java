68608d52882b0485c6ba1fad5ce7b32051800353
jakepardue <jjpardue@wpi.edu>
2017-12-11 01:00:21 -0500

    Creating Spiritual staff and request objects

---
+++ src/main/java/com/teama/requestsubsystem/spiritualcarefeature/SpiritualService.java
@@ -0,0 +1,24 @@
+package com.teama.requestsubsystem.spiritualcarefeature;
+
+/**
+ * Created by jakepardue on 12/10/17.
+ */
+public enum SpiritualService {
+    BAPTISMS("Baptisms"), BLESSINGS("Blessings"), MEMORIALSERVICES("Memorial Services"),
+    MARRIAGES("Marriages"), ENDOFLIFERITUALS("End of Life Rituals"), CONSULTATIONS("Consultations"),
+    CODEBLUE("Code Blue"),CODETRAUMA("Code Trauma");
+
+    private String type;
+    SpiritualService(String s){
+        this.type = s;
+    }
+
+    public static SpiritualService getSpiritualService(String str) throws IllegalAccessException {
+        for(SpiritualService s: SpiritualService.values()){
+            if(s.toString().equals(str)){
+                return s;
+            }
+        }
+        throw new IllegalAccessException("No such service exists: " + str);
+    }
+}
