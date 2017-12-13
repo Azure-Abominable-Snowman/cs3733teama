e5d11f433ce11f1443c68f38cdd1b51d1b7004a6
mebroughton <meghanbroughton@gmail.com>
2017-12-13 07:09:21 -0500

    help page update

--- src/main/java/com/teama/refresh/RefreshToMain.java
+++ src/main/java/com/teama/refresh/RefreshToMain.java
@@ -2,15 +2,17 @@ package com.teama.refresh;
 
 
 import java.awt.event.InputEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 import java.util.ArrayList;
 
 import com.teama.controllers.MainScreenController;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import org.w3c.dom.events.MouseEvent;
 
 
-public class RefreshToMain {
+public class RefreshToMain implements MouseListener {
     private List<MementoDesign.Memento> savedStates = new ArrayList<>();
     public void addMemento(MementoDesign.Memento m){
         savedStates.add(m);
@@ -24,8 +26,7 @@ public class RefreshToMain {
         return null;
     }
 
-
-    public MementoDesign.Memento Refresh(InputEvent interaction) {
+    public MementoDesign.Memento Refresh() {
         this.primaryStage = setPrimary();
 
         MementoDesign mementoDesign = new MementoDesign();
@@ -34,7 +35,8 @@ public class RefreshToMain {
         mementoDesign.setState(currStage);
         savedStates.add(mementoDesign.saveState());
 
-        while (interaction == null) {
+
+        while (MouseEvent == null) {
             try {
                 interaction.wait(1000);
             } catch (InterruptedException ie) {
