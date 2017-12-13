da445468205338b9d84c907be06bb1c04c9296f5
Leo Chen <qchen3@gmail.com>
2017-12-12 20:23:58 -0500

    Made the new function to return the list of floors that it went through.

--- src/main/java/com/teama/mapsubsystem/pathfinding/TextDirections.java
+++ src/main/java/com/teama/mapsubsystem/pathfinding/TextDirections.java
@@ -1,5 +1,8 @@
 package com.teama.mapsubsystem.pathfinding;
 
+import com.teama.mapsubsystem.data.Floor;
+import com.teama.mapsubsystem.data.NodeType;
+
 import java.util.ArrayList;
 
 /**
@@ -14,5 +17,22 @@ public class TextDirections {
 
     public ArrayList<Direction> getDirections() {
         return directions;
+    }
+
+    public ArrayList<Floor> getFloorDirections()
+    {
+        ArrayList<Floor> result = new ArrayList<>();
+        result.add(directions.get(0).getStart().getLevel()); // add in the starting floor.
+        for(int i=1;i<directions.size();++i)
+        {
+            Direction thisdir = directions.get(i);
+            String thisturn = thisdir.getTurn().toString();
+            if(thisturn.equals(TurnType.ELEVATOR.toString())
+                    || thisturn.equals(TurnType.STAIR))
+            {
+                result.add(thisdir.getEnd().getLevel()); // add the floor of the end once.
+            }
+        }
+        return result;
     }
 }
